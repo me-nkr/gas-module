338c073a2ed7db69fd83e293dc94d9e5426bb0a9
Naveen K R <thenaveenkr7@gmail.com>
2021-11-23 16:37:17 +0530

    Rename file

---
+++ src/requireFile.js
@@ -0,0 +1,8 @@
+/**
+ * requireFile - convert a google app script file into a javascript module
+ * @param {String} file - path of file to be converted into module
+ * @returns {Object}
+ */
+module.exports = (file) => {
+
+}
