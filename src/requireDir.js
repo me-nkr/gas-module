34a558ffcb7072e04fa3fbc9f754f7c9b198266a
Naveen K R <thenaveenkr7@gmail.com>
2021-11-23 16:30:50 +0530

    Create module descriptions

---
+++ src/requireDir.js
@@ -0,0 +1,9 @@
+/**
+ * requireDir - convert google app script files in the directory into a javascript module
+ * @param {String} directory - path of directory to be converted into module
+ * @param {Boolean} recursion - decides to ignore or consider subdirectories
+ * @returns {Object}
+ */
+module.exports = (directory, recursion) => {
+
+}
