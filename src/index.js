035dd1fc4d911ffedc43e033e1b17bf79faa9efa
Naveen K R <thenaveenkr7@gmail.com>
2021-11-23 16:36:22 +0530

    Minor Update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const requireFile = require('./requireFile');
+const requireDir = require('./requireDir');
+
+module.exports = {
+    requireDir,
+    requireFile
+}
--- src/require.js
+++
@@ -1,8 +0,0 @@
-/**
- * require - convert a google app script file into a javascript module
- * @param {String} file - path of file to be converted into module
- * @returns {Object}
- */
-module.exports = (file) => {
-
-}
