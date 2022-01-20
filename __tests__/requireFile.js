0add7f416cbbf6882bf150a6be9d7042f4867956
Naveen K R <thenaveenkr7@gmail.com>
2022-01-20 19:03:10 +0530

    WIP | add tests to requireFile

--- __tests__/requireFile.js
+++ __tests__/requireFile.js
@@ -2,13 +2,31 @@ const { requireFile } = require('../src');
 
 describe('Single File', () => {
     it('should throw error if file name is not a string', () => {
-       expect(() => requireFile(25)).toThrowError('fileName: Expected string, number given'); 
-       expect(() => requireFile()).toThrowError('fileName: Expected string, undefined given'); 
-       expect(() => requireFile([])).toThrowError('fileName: Expected string, object given'); 
-       expect(() => requireFile({})).toThrowError('fileName: Expected string, object given'); 
-       expect(() => requireFile(true)).toThrowError('fileName: Expected string, boolean given'); 
-       expect(() => requireFile(false)).toThrowError('fileName: Expected string, boolean given'); 
+        expect(() => requireFile(25)).toThrowError('fileName: Expected string, number given');
+        expect(() => requireFile()).toThrowError('fileName: Expected string, undefined given');
+        expect(() => requireFile([])).toThrowError('fileName: Expected string, object given');
+        expect(() => requireFile({})).toThrowError('fileName: Expected string, object given');
+        expect(() => requireFile(true)).toThrowError('fileName: Expected string, boolean given');
+        expect(() => requireFile(false)).toThrowError('fileName: Expected string, boolean given');
     });
+    it('should throw error if options is not an object or undefined', () => {
+        expect(() => requireFile('./__tests__/mocks/one.js', 25)).toThrowError('options: Expected object, number given');
+        expect(() => requireFile('./__tests__/mocks/one.js', [])).toThrowError('options: Expected object, array given');
+        expect(() => requireFile('./__tests__/mocks/one.js', '')).toThrowError('options: Expected object, string given');
+        expect(() => requireFile('./__tests__/mocks/one.js', true)).toThrowError('options: Expected object, boolean given');
+        expect(() => requireFile('./__tests__/mocks/one.js', false)).toThrowError('options: Expected object, boolean given');
+        expect(() => requireFile('./__tests__/mocks/one.js')).not.toThrowError();
+    });
+
+    it('should throw error if options.mocks is not an object or undefined', () => {
+        expect(() => requireFile('./__tests__/mocks/one.js', { mocks: 'mock' })).toThrowError('mocks: Expected object, string given');
+        expect(() => requireFile('./__tests__/mocks/one.js', { mocks: 25 })).toThrowError('mocks: Expected object, number given');
+        expect(() => requireFile('./__tests__/mocks/one.js', { mocks: [] })).toThrowError('mocks: Expected object, array given');
+        expect(() => requireFile('./__tests__/mocks/one.js', { mocks: true })).toThrowError('mocks: Expected object, boolean given');
+        expect(() => requireFile('./__tests__/mocks/one.js', { mocks: false })).toThrowError('mocks: Expected object, boolean given');
+        expect(() => requireFile('./__tests__/mocks/one.js', { mocks: undefined })).not.toThrowError();
+    });
+
     it('should throw error on non \'.js\' or \'.ts\' file name', () => {
         expect(() => requireFile('test.txt')).toThrowError('fileName: Invalid file type');
         expect(() => requireFile('test.json')).toThrowError('fileName: Invalid file type');
@@ -17,7 +35,7 @@ describe('Single File', () => {
     it('should throw error when given file does\'nt exist', () => {
         expect(() => requireFile('nonExisting.js')).toThrowError('fileName: File not found')
     });
-    
+
     describe('On Success', () => {
         const oneResult = requireFile('./__tests__/mocks/one.js');
         const twoResult = requireFile('./__tests__/mocks/two.js');
