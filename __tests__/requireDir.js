82477db94777ce490019225238565440526010ea
Naveen K R <thenaveenkr7@gmail.com>
2022-01-20 13:04:48 +0530

    WIP | add tests

--- __tests__/requireDir.js
+++ __tests__/requireDir.js
@@ -44,7 +44,7 @@ describe('Directory', () => {
         expect(() => requireDir('./__tests__/mocks/validDir', false, { filterExtension: [] })).toThrowError('options: invalid file type');
         expect(() => requireDir('./__tests__/mocks/validDir', false, { filterExtension: [[]] })).toThrowError('options: invalid file type');
         expect(() => requireDir('./__tests__/mocks/validDir', false, { filterExtension: ['.js', '.ts', '.ss'] })).toThrowError('options: invalid file type');
-        expect(() => requireDir('./__tests__/mocks/validDir', false, { filterExtension: undefined})).not.toThrowError();
+        expect(() => requireDir('./__tests__/mocks/validDir', false, { filterExtension: undefined })).not.toThrowError();
     });
 
     describe('On Sucess', () => {
@@ -88,7 +88,7 @@ describe('Directory', () => {
                 dos: 'function dos() {\n    console.log(\'dos\');\n}'
             }
 
-           it('should not have functions in the file in the subdirectory in the returning object', () => {
+            it('should not have functions in the file in the subdirectory in the returning object', () => {
                 expect(result).toHaveProperty('nada');
                 expect(result.nada.toString()).toBe(zerots);
                 expect(result).toHaveProperty('uno');
@@ -102,7 +102,80 @@ describe('Directory', () => {
             });
         });
 
-        describe('FileExtension is \'.js\'', () => {
+        describe('Recursion is undefined', () => {
+            const result = requireDir('./__tests__/mocks/validDir', undefined, { filterExtension: ['.js', '.ts'] });
+            const zerots = 'function nada() {\n    console.log(\'nada\');\n}';
+            const onejs = 'function uno() {\n    console.log(\'uno\');\n}';
+            const twojs = {
+                duo: 'function duo() {\n    console.log(\'duo\');\n}',
+                dos: 'function dos() {\n    console.log(\'dos\');\n}'
+            }
+
+            it('should not have functions in the file in the subdirectory in the returning object', () => {
+                expect(result).toHaveProperty('nada');
+                expect(result.nada.toString()).toBe(zerots);
+                expect(result).toHaveProperty('uno');
+                expect(result.uno.toString()).toBe(onejs);
+                expect(result).toHaveProperty('duo');
+                expect(result).toHaveProperty('dos');
+                expect(result.duo.toString()).toBe(twojs.duo);
+                expect(result.dos.toString()).toBe(twojs.dos);
+                expect(result).not.toHaveProperty('theen');
+                expect(result).not.toHaveProperty('quad');
+            });
+        });
+
+        describe('Recursion and options is undefined', () => {
+            const result = requireDir('./__tests__/mocks/validDir');
+            const onejs = 'function uno() {\n    console.log(\'uno\');\n}';
+            const twojs = {
+                duo: 'function duo() {\n    console.log(\'duo\');\n}',
+                dos: 'function dos() {\n    console.log(\'dos\');\n}'
+            }
+
+            it('should have functions in the files from given directory in the returning object', () => {
+                expect(result).toHaveProperty('uno');
+                expect(result.uno.toString()).toBe(onejs);
+                expect(result).toHaveProperty('duo');
+                expect(result).toHaveProperty('dos');
+                expect(result.duo.toString()).toBe(twojs.duo);
+                expect(result.dos.toString()).toBe(twojs.dos);
+
+            })
+
+            it('should not have functions in the file in the subdirectory or \'.ts\' files in the returning object', () => {
+                expect(result).not.toHaveProperty('nada');
+                expect(result).not.toHaveProperty('theen');
+                expect(result).not.toHaveProperty('quad');
+            });
+        });
+
+        describe('Options is undefined', () => {
+            const result = requireDir('./__tests__/mocks/validDir', true, undefined);
+            const onejs = 'function uno() {\n    console.log(\'uno\');\n}';
+            const twojs = {
+                duo: 'function duo() {\n    console.log(\'duo\');\n}',
+                dos: 'function dos() {\n    console.log(\'dos\');\n}'
+            }
+            const threejs = 'function theen() {\n    console.log(\'theen\');\n}';
+
+            it('should have functions from \'.js\' files', () => {
+                expect(result).toHaveProperty('uno');
+                expect(result.uno.toString()).toBe(onejs);
+                expect(result).toHaveProperty('duo');
+                expect(result).toHaveProperty('dos');
+                expect(result.duo.toString()).toBe(twojs.duo);
+                expect(result.dos.toString()).toBe(twojs.dos);
+                expect(result).toHaveProperty('theen');
+                expect(result.theen.toString()).toBe(threejs);
+            });
+            it('should not have functions from \'.ts\' files', () => {
+                expect(result).not.toHaveProperty('nada');
+                expect(result).not.toHaveProperty('quad');
+            });
+        });
+
+        describe('FilterExtension is \'.js\'', () => {
             const result = requireDir('./__tests__/mocks/validDir', true, { filterExtension: ['.js'] });
             const onejs = 'function uno() {\n    console.log(\'uno\');\n}';
             const twojs = {
@@ -127,7 +200,7 @@ describe('Directory', () => {
             });
         });
 
-        describe('FileExtension is \'.ts\'', () => {
+        describe('FilterExtension is \'.ts\'', () => {
             const result = requireDir('./__tests__/mocks/validDir', true, { filterExtension: ['.ts'] });
             const zerots = 'function nada() {\n    console.log(\'nada\');\n}';
             const fourts = 'function quad() {\n    console.log(\'quad\');\n}';
@@ -145,7 +218,7 @@ describe('Directory', () => {
             });
         });
 
-        describe('FileExtension is \'.js\' and \'.ts\'', () => {
+        describe('FilterExtension is \'.js\' and \'.ts\'', () => {
             const result = requireDir('./__tests__/mocks/validDir', true, { filterExtension: ['.js', '.ts'] });
             const zerots = 'function nada() {\n    console.log(\'nada\');\n}';
             const onejs = 'function uno() {\n    console.log(\'uno\');\n}';
@@ -168,7 +241,7 @@ describe('Directory', () => {
                 expect(result.theen.toString()).toBe(threejs);
                 expect(result).toHaveProperty('quad');
                 expect(result.quad.toString()).toBe(fourts);
- 
+
             });
         });
 
