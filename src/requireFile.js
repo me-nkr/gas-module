f4b94fbc1f913937f886acb5c6c649fafeb791e4
Naveen K R <thenaveenkr7@gmail.com>
2022-01-20 19:19:01 +0530

    WIP | add more error handlers to requireFile

--- src/requireFile.js
+++ src/requireFile.js
@@ -1,12 +1,15 @@
 /**
  * requireFile - convert a google app script file into a javascript module
  * @param {String} file - path of file to be converted into module
+ * @param {Object} options - { mocks: [] }
  * @returns {Object}
  */
 const fs = require('fs');
 const vm = require('vm');
-module.exports = (file) => {
+module.exports = (file, options = {}) => {
     if (typeof file !== 'string') throw Error(`fileName: Expected string, ${typeof file} given`);
+    if (typeof options !== 'object' || options instanceof Array) throw Error(`options: Expected object, ${options instanceof Array ? 'array' : typeof options} given`);
+    if (options.mocks !== undefined && (typeof options.mocks !== 'object' || options.mocks instanceof Array)) throw Error(`mocks: Expected object, ${options.mocks instanceof Array ? 'array' : typeof options.mocks} given`);
     if (!/(\.js|\.ts)$/.test(file)) throw Error('fileName: Invalid file type');
     try {
         const code = fs.readFileSync(file, 'utf8');
